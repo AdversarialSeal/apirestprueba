2149ba6a1bff77cd1afbbc12852f60db78e599fe
Kevin Edilson Valencia Romero <valenciasep95@gmail.com>
2024-02-02 13:08:45 -0500

    adicion mensaje servicio ME

--- src/routes/endpoint.me.js
+++ src/routes/endpoint.me.js
@@ -1,3 +1,23 @@
 app.get('/api/me', authenticate, (req, res) => {
     res.json({ user: req.user });
- });
+
+    // Construye una respuesta HTML simple con la información del usuario
+   const htmlResponse = `
+   <html>
+      <head>
+         <title>Mi Perfil</title>
+      </head>
+      <body>
+         <h1>Mi Perfil</h1>
+         <p>Nombre de usuario: ${user.username}</p>
+         <p>Correo electrónico: ${user.email}</p>
+         <p>Nombre: ${user.firstName} ${user.lastName}</p>
+      </body>
+   </html>
+`;
+
+// Envía la respuesta HTML al usuario
+res.send(htmlResponse);
+});
+
+ 
